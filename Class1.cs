0b0ca6b260ef3d6a8e216f0703cfb9801ded291d
madhavilathar21 <38210429+madhavilathar21@users.noreply.github.com>
2021-05-20 22:41:32 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿ using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	
+     public SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|Database1.mdf;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework");
+		
+    public Boolean  ins_up_del(string query)
+
+    {
+        SqlCommand cmd = new SqlCommand(query, con);
+        try
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+            return true;
+        }
+
+        catch(Exception ex)
+        {
+            return false;
+        }
+
+    }
+    public DataSet selectData(string query)
+    {
+        SqlDataAdapter adp = new SqlDataAdapter(query, con);
+        DataSet ds = new DataSet();
+        try
+        {
+            adp.Fill(ds);
+            return ds;
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+		// TODO: Add constructor logic here
+		//
+	
+}
